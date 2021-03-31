893c4bbe33afb22379358c96ffc89595f248844a
Matt Jarvis <matt.jarvis@gmail.com>
2021-03-31 11:46:28 +0100

    AEA-950: Move fixtures to conftest.py. Parametrise tests which differ only by the endpoint.

--- tests/test_key_locator.py
+++ tests/test_key_locator.py
@@ -1,253 +1,78 @@
-import asyncio
-import os
-
 import pytest
 import requests
 from api_test_utils.apigee_api_apps import ApigeeApiDeveloperApps
-from api_test_utils.apigee_api_products import ApigeeApiProducts
 
-JWKS_URL = "https://raw.githubusercontent.com/NHSDigital/identity-service-jwks" \
-    "/main/jwks/internal-dev/278af795-6884-42dd-a713-f2b03927abb5.json"
-ENVIRONMENT = os.environ["APIGEE_ENVIRONMENT"]
-BASE_PATH = os.environ["SERVICE_BASE_PATH"]
-
-
-@pytest.fixture(scope="module")
-def event_loop():
-    loop = asyncio.get_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest.fixture(scope='module')
-def apigee_root_url():
-    return "https://api.service.nhs.uk" if ENVIRONMENT == "prod" \
-        else f"https://{ENVIRONMENT}.api.service.nhs.uk"
-
-
-@pytest.fixture(scope='module')
-def service_root_url(apigee_root_url):
-    return f"{apigee_root_url}/{BASE_PATH}"
-
-
-@pytest.fixture(scope='module')
-def apigee_organization():
-    return "nhsd-prod" if ENVIRONMENT in ["int", "prod"] else "nhsd-nonprod"
-
-
-@pytest.fixture(scope='module')
-@pytest.mark.asyncio
-async def test_product(apigee_organization):
-    api = ApigeeApiProducts(org_name=apigee_organization)
-    await api.create_new_product()
-    yield api
-    await api.destroy_product()
-
-
-@pytest.fixture(scope='module')
-@pytest.mark.asyncio
-async def test_product_other_environment():
-    api = ApigeeApiProducts(org_name="nhsd-nonprod")
-    await api.create_new_product()
-    yield api
-    await api.destroy_product()
-
-
-@pytest.fixture(scope='module')
-@pytest.mark.asyncio
-async def test_app(apigee_organization, test_product):
-    api = ApigeeApiDeveloperApps(org_name=apigee_organization)
-    await api.create_new_app()
-    yield api
-    await api.destroy_app()
-
-
-@pytest.fixture(scope='module')
-@pytest.mark.asyncio
-async def test_app_no_product_subscriptions(apigee_organization):
-    api = ApigeeApiDeveloperApps(org_name=apigee_organization)
-    await api.create_new_app()
-    yield api
-    await api.destroy_app()
-
-
-@pytest.fixture(scope='module')
-@pytest.mark.asyncio
-async def test_app_no_jwks_url(apigee_organization, test_product):
-    api = ApigeeApiDeveloperApps(org_name=apigee_organization)
-    await api.create_new_app()
-    yield api
-    await api.destroy_app()
-
-
-@pytest.fixture(scope='module')
-@pytest.mark.asyncio
-async def test_app_other_environment(test_product_other_environment):
-    api = ApigeeApiDeveloperApps(org_name="nhsd-nonprod")
-    await api.create_new_app()
-    yield api
-    await api.destroy_app()
-
-
-@pytest.fixture(scope='module', autouse=True)
-@pytest.mark.asyncio
-async def add_jwks_url_to_app(test_app: ApigeeApiDeveloperApps):
-    await test_app.set_custom_attributes({
-        "jwks-resource-url": JWKS_URL
-    })
-
-
-@pytest.fixture(scope='module', autouse=True)
-@pytest.mark.asyncio
-async def add_jwks_url_to_other_environment_app(test_app: ApigeeApiDeveloperApps):
-    await test_app.set_custom_attributes({
-        "jwks-resource-url": JWKS_URL
-    })
-
-
-@pytest.fixture(scope='module', autouse=True)
-@pytest.mark.asyncio
-async def add_product_to_app(test_product: ApigeeApiProducts, test_app: ApigeeApiDeveloperApps):
-    await test_app.add_api_product([test_product.name])
-
-
-@pytest.fixture(scope='module', autouse=True)
-@pytest.mark.asyncio
-async def add_product_to_no_jwks_url_app(test_product: ApigeeApiProducts, test_app_no_jwks_url: ApigeeApiDeveloperApps):
-    await test_app_no_jwks_url.add_api_product([test_product.name])
-
-
-@pytest.fixture(scope='module', autouse=True)
-@pytest.mark.asyncio
-async def add_product_to_other_environment_app(
-        test_product_other_environment: ApigeeApiProducts,
-        test_app_other_environment: ApigeeApiDeveloperApps
-):
-    await test_app_other_environment.add_api_product([test_product_other_environment.name])
-
-
-@pytest.fixture(scope='module', autouse=True)
-@pytest.mark.asyncio
-async def set_product_environment(test_product: ApigeeApiProducts):
-    await test_product.update_environments([ENVIRONMENT])
-
-
-@pytest.fixture(scope='module', autouse=True)
-@pytest.mark.asyncio
-async def set_other_environment_product_environment(test_product_other_environment: ApigeeApiProducts):
-    other_environment = "internal-dev" if ENVIRONMENT == "internal-qa" else "internal-qa"
-    await test_product_other_environment.update_environments([other_environment])
-
-
-@pytest.fixture(autouse=True)
-@pytest.mark.asyncio
-async def sleep_before_test():
-    """ sleep for 200 millis before each test to avoid hitting the SpikeArrest policy """
-    await asyncio.sleep(0.2)
+import conftest
 
 
 @pytest.mark.asyncio
-async def test_jwks_endpoint_valid_request(service_root_url, test_app: ApigeeApiDeveloperApps):
-    client_id = test_app.client_id
+async def test_jwks_endpoint_valid_request(service_root_url, test_app_valid_config: ApigeeApiDeveloperApps):
+    client_id = test_app_valid_config.client_id
     response = requests.get(f"{service_root_url}/jwks?clientId={client_id}")
+    assert response.status_code == 200
     assert ("keys" in response.json())
 
 
 @pytest.mark.asyncio
-async def test_jwks_endpoint_no_client_id(service_root_url):
-    response = requests.get(f"{service_root_url}/jwks")
+async def test_jku_endpoint_valid_request(service_root_url, test_app_valid_config: ApigeeApiDeveloperApps):
+    client_id = test_app_valid_config.client_id
+    response = requests.get(f"{service_root_url}/jku?clientId={client_id}")
+    assert response.status_code == 200
+    assert (response.text == conftest.JWKS_URL)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("endpoint", ["jku", "jwks"])
+async def test_no_client_id(service_root_url, endpoint):
+    response = requests.get(f"{service_root_url}/{endpoint}")
     assert response.status_code == 400
 
 
 @pytest.mark.asyncio
-async def test_jwks_endpoint_empty_client_id(service_root_url):
-    response = requests.get(f"{service_root_url}/jwks?clientId=")
+@pytest.mark.parametrize("endpoint", ["jku", "jwks"])
+async def test_empty_client_id(service_root_url, endpoint):
+    response = requests.get(f"{service_root_url}/{endpoint}?clientId=")
     assert response.status_code == 400
 
 
 @pytest.mark.asyncio
-async def test_jwks_endpoint_invalid_client_id(service_root_url):
-    response = requests.get(f"{service_root_url}/jwks?clientId=notAValidClientId")
+@pytest.mark.parametrize("endpoint", ["jku", "jwks"])
+async def test_invalid_client_id(service_root_url, endpoint):
+    response = requests.get(f"{service_root_url}/{endpoint}?clientId=notAValidClientId")
     assert response.status_code == 404
 
 
 @pytest.mark.asyncio
-async def test_jwks_endpoint_no_product_subscriptions(
+@pytest.mark.parametrize("endpoint", ["jku", "jwks"])
+async def test_no_jwks_url_for_app(
         service_root_url,
-        test_app_no_product_subscriptions: ApigeeApiDeveloperApps
-):
-    client_id = test_app_no_product_subscriptions.client_id
-    response = requests.get(f"{service_root_url}/jwks?clientId={client_id}")
-    assert response.status_code == 404
-
-
-@pytest.mark.asyncio
-async def test_jwks_endpoint_no_jwks_url_for_app(
-        service_root_url,
+        endpoint,
         test_app_no_jwks_url: ApigeeApiDeveloperApps
 ):
     client_id = test_app_no_jwks_url.client_id
-    response = requests.get(f"{service_root_url}/jwks?clientId={client_id}")
+    response = requests.get(f"{service_root_url}/{endpoint}?clientId={client_id}")
     assert response.status_code == 404
 
 
 @pytest.mark.asyncio
-async def test_jwks_endpoint_app_not_for_this_environment(
+@pytest.mark.parametrize("endpoint", ["jku", "jwks"])
+async def test_no_product_subscriptions(
         service_root_url,
-        test_app_other_environment: ApigeeApiDeveloperApps
-):
-    client_id = test_app_other_environment.client_id
-    response = requests.get(f"{service_root_url}/jwks?clientId={client_id}")
-    assert response.status_code == 404
-
-
-@pytest.mark.asyncio
-async def test_jku_endpoint_valid_request(service_root_url, test_app: ApigeeApiDeveloperApps):
-    client_id = test_app.client_id
-    response = requests.get(f"{service_root_url}/jku?clientId={client_id}")
-    assert (response.text == JWKS_URL)
-
-
-@pytest.mark.asyncio
-async def test_jku_endpoint_no_client_id(service_root_url):
-    response = requests.get(f"{service_root_url}/jku")
-    assert response.status_code == 400
-
-
-@pytest.mark.asyncio
-async def test_jku_endpoint_empty_client_id(service_root_url):
-    response = requests.get(f"{service_root_url}/jku?clientId=")
-    assert response.status_code == 400
-
-
-@pytest.mark.asyncio
-async def test_jku_endpoint_invalid_client_id(service_root_url):
-    response = requests.get(f"{service_root_url}/jku?clientId=notAValidClientId")
-    assert response.status_code == 404
-
-
-@pytest.mark.asyncio
-async def test_jku_endpoint_no_product_subscriptions(
-        service_root_url,
+        endpoint,
         test_app_no_product_subscriptions: ApigeeApiDeveloperApps
 ):
     client_id = test_app_no_product_subscriptions.client_id
-    response = requests.get(f"{service_root_url}/jku?clientId={client_id}")
+    response = requests.get(f"{service_root_url}/{endpoint}?clientId={client_id}")
     assert response.status_code == 404
 
 
 @pytest.mark.asyncio
-async def test_jku_endpoint_no_jwks_url_for_app(service_root_url, test_app_no_jwks_url: ApigeeApiDeveloperApps):
-    client_id = test_app_no_jwks_url.client_id
-    response = requests.get(f"{service_root_url}/jku?clientId={client_id}")
-    assert response.status_code == 404
-
-
-@pytest.mark.asyncio
-async def test_jku_endpoint_app_not_for_this_environment(
+@pytest.mark.parametrize("endpoint", ["jku", "jwks"])
+async def test_app_not_for_this_environment(
         service_root_url,
+        endpoint,
         test_app_other_environment: ApigeeApiDeveloperApps
 ):
     client_id = test_app_other_environment.client_id
-    response = requests.get(f"{service_root_url}/jku?clientId={client_id}")
+    response = requests.get(f"{service_root_url}/{endpoint}?clientId={client_id}")
     assert response.status_code == 404
